24e870fe56e5b3b0b43ed441f1afefaad748739f
azrael28gmail <91268618+azrael28gmail@users.noreply.github.com>
2021-10-04 18:30:46 +0500

    Update functions.py

--- functions.py
+++ functions.py
@@ -62,6 +62,15 @@ async def clear_file(filename, reponame):
     contents = repo.get_contents(filename)
     repo.update_file(contents.path, "None", "None", contents.sha)
 
+async def create_file(filename, reponame):
+    filename = "/" + filename
+    g = Github(GB_TOKEN)
+
+    repo = g.get_user().get_repo(reponame)
+    contents = repo.get_contents(filename)
+    repo.create_file(contents.path, "None", "None")
+
+
 
 async def get_page(url):
     page = requests.get(url)
